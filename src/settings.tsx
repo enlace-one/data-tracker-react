332ec7001c4826a29a621c93e365663708d7038d
A-Management <148012721+A-Management@users.noreply.github.com>
2025-04-06 16:31:24 -0500

    Added a bunch of *-black topics

--- src/settings.tsx
+++ src/settings.tsx
@@ -57,20 +57,46 @@ export const DEFAULT_TOPICS = [
   { name: "Book (Green)", imageLink: "book-green.svg" },
   { name: "Book (Purple)", imageLink: "book-purple.svg" },
   { name: "Book (Red)", imageLink: "book-red.svg" },
+  { name: "Camera (Black)", imageLink: "camera-black.svg" },
   { name: "Car (Black)", imageLink: "car-black.svg" },
+  { name: "Cart (Black)", imageLink: "cart-black.svg" },
+  { name: "Cat (Black)", imageLink: "cat-black.svg" },
+  { name: "Clock (Black)", imageLink: "clock-black.svg" },
   { name: "Coffee (Black)", imageLink: "coffee-black.svg" },
   { name: "Color (Black)", imageLink: "color-black.svg" },
-  { name: "Data Tracker", imageLink: "dataTracker.svg" },
+  { name: "Color (Blue)", imageLink: "color-blue.svg" },
+  { name: "Color (Cyan)", imageLink: "color-cyan.svg" },
+  { name: "Color (Green)", imageLink: "color-green.svg" },
+  { name: "Color (Purple)", imageLink: "color-purple.svg" },
+  { name: "Color (Red)", imageLink: "color-red.svg" },
+  { name: "Dog (Black)", imageLink: "dog-black.svg" },
+  { name: "Earth (Black)", imageLink: "earth-black.svg" },
+  { name: "File (Black)", imageLink: "file-black.svg" },
+  { name: "Gears (Black)", imageLink: "gears-black.svg" },
   { name: "Gift (Black)", imageLink: "gift-black.svg" },
+  { name: "Globe (Black)", imageLink: "globe-black.svg" },
   { name: "Guitar (Black)", imageLink: "guitar-black.svg" },
-  { name: "Profile", imageLink: "profile-view.svg" }, // Not a "view" per your rule?
+  { name: "Heart (Black)", imageLink: "heart-black.svg" },
+  { name: "House (Black)", imageLink: "house-black.svg" },
+  { name: "Music (Black)", imageLink: "music-black.svg" },
+  { name: "Pin (Black)", imageLink: "pin-black.svg" },
+  { name: "Plane (Black)", imageLink: "plane-black.svg" },
+  { name: "Rain (Black)", imageLink: "rain-black.svg" },
   { name: "Shirt (Black)", imageLink: "shirt-black.svg" },
   { name: "Shirt (Blue)", imageLink: "shirt-blue.svg" },
   { name: "Shirt (Cyan)", imageLink: "shirt-cyan.svg" },
   { name: "Shirt (Green)", imageLink: "shirt-green.svg" },
   { name: "Shirt (Purple)", imageLink: "shirt-purple.svg" },
   { name: "Shirt (Red)", imageLink: "shirt-red.svg" },
+  { name: "Temp (Black)", imageLink: "temp-black.svg" },
+  { name: "Treadmil (Black)", imageLink: "treadmil-black.svg" },
+  { name: "Tree (Black)", imageLink: "tree-black.svg" },
+  { name: "Work (Black)", imageLink: "work-black.svg" },
 ];
+
+////////////
+// Styles //
+////////////
 
 // Black
 // #2b2b2b (black)
